929b0f63b6780f3bfc4812e129d0b76e0f59b388
Jhonny Li <jhonny@jhonny.se>
2019-03-10 01:07:29 +0100

    Initial commit

---
+++ ProxyHttpClient/ProxyHttpClient/ErrorHandling/ProxyExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyHttpClient.ErrorHandling
+{
+    [Serializable]
+    public sealed class ProxyUriException : Exception
+    {
+        public ProxyUriException()
+        {
+        }
+
+        public ProxyUriException(string message) : base(message)
+        {
+        }
+
+        public ProxyUriException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ ProxyHttpClient/ProxyHttpClient/Models/ProxyClientConfig.cs
@@ -0,0 +1,34 @@
+﻿using ProxyHttpClient.ErrorHandling;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace ProxyHttpClient.Models
+{
+    /// <summary>
+    /// Client settings
+    /// Target url etc
+    /// </summary>
+    public partial class ProxyClientConfig
+    {
+        internal Uri _targetUri;
+
+        public ProxyClientConfig()
+        {
+            try
+            {
+#if RELEASE
+                _targetUri = new Uri(ConfigurationManager.AppSettings["ProxyClientUrl"]);
+#else
+                _targetUri = new Uri("http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy");
+#endif
+            }
+            catch (ProxyUriException pue)
+            {
+                throw pue;
+            }
+        }
+
+    }
+}
